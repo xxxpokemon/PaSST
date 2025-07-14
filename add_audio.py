04eb5dcd6e1dfad389e06ebe0836e1eb7a06a172
C.L.M. Marosvölgyi <c.l.m.marosvolgyi@umail.leidenuniv.nl>
2025-07-14 11:50:38 +0200

    ritsel

---
+++ add_audio.py
@@ -0,0 +1,6 @@
+names = ['rustling', 'utensils', 'pen_clicking', 'clearing_throat', 'sniffing', 'cracking_joints', 'background', 'humming']
+for i in range(1,9):
+    for ii in range(1,41):
+        num = str(ii) if ii > 9 else "0" + str(ii)
+        line = f"6-300{i}{num}-A-{i + 50}.wav,6,{i + 50},{names[i - 1]},False,300{i}{num},A"
+        print(line)
