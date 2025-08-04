319aa9b6a8cdc961573ff4e43601b69e5534b0e5
C.L.M. Marosvölgyi <c.l.m.marosvolgyi@umail.leidenuniv.nl>
2025-08-04 12:51:27 +0200

    segments 0.5 1 en 2 en python voor inkorten

---
+++ extract_audio_fragment.py
@@ -0,0 +1,30 @@
+import librosa
+import soundfile as sf
+import numpy as np
+import os
+
+audio_dir = 'test_audio'
+output_dir = 'segments_2'
+
+def extract_peak_segment(audio_path, output_path, pre_peak=0.5, post_peak=1.5):
+    audio, sr = librosa.load(audio_path, sr=None)
+    audio_length = len(audio)
+    segment_length = int((pre_peak + post_peak) * sr)
+    peak_index = np.argmax(np.abs(audio))
+
+    start_index = min(int(peak_index - pre_peak * sr), audio_length - segment_length)
+    end_index = max(int(peak_index + post_peak * sr), segment_length)
+
+    start_index = max(start_index, 0)
+    end_index = min(end_index, audio_length)
+
+    segment = audio[start_index:end_index]
+    sf.write(output_path, segment, sr)
+
+for file in os.listdir(audio_dir):
+    print('Processing file: ', file)
+    audio_path = os.path.join(audio_dir, file)
+    output_path = os.path.join(output_dir, file)
+    if audio_path[-4:] == '.wav':
+        extract_peak_segment(audio_path, output_path)
+    print('Exported to: ', output_path)
