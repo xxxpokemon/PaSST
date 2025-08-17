9272e141bbffc4e436017671c02e9022e5737c01
C.L.M. Marosvölgyi <c.l.m.marosvolgyi@umail.leidenuniv.nl>
2025-08-17 21:21:38 +0200

    last training

--- split_train_val_test.py
+++ split_train_val_test.py
@@ -2,16 +2,23 @@ import os
 import random as rd
 import shutil
 
-train_val_dir = "esc50_extra/train_val_audio"
-test_dir = "test_audio"
+train_val_dir = "esc50_extra/bahrei_sounds"
+test_dir = "esc50_extra/bahrei_sounds"
 complete_audio_dir = "esc50_extra/audio_32k"
-target_classes = [23, 24, 28, 29, 31, 32, 38, 50, 51, 52, 53, 54, 55, 56, 57, 58]
+target_classes = [23, 24, 28, 29, 31, 32, 38, 50]
 
 num_of_total_classes = 59
 samples_per_class = 40
-ratio_test_total = 0.4
+ratio_test_total = 0
 test_per_class = int(samples_per_class * ratio_test_total)
 train_val_per_class = samples_per_class - test_per_class
+
+# for file in os.listdir(complete_audio_dir):
+#     category = file[-6:-4]
+#     cat_int = int(category) if category[0] != '-' else int(category[1])
+#     if cat_int in target_classes:
+#         audio_path = os.path.join(complete_audio_dir, file)
+#         shutil.copy(audio_path, test_dir)
 
 test_dir_counters = [0] * num_of_total_classes
 total_counters = [0] * num_of_total_classes
