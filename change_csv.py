1037d618552bd765376aff4e2e43a2b1a4b30587
C.L.M. Marosvölgyi <c.l.m.marosvolgyi@umail.leidenuniv.nl>
2025-08-17 21:32:25 +0200

    last training

--- change_csv.py
+++ change_csv.py
@@ -2,7 +2,7 @@ import pandas as pd
 import os
 
 # Stap 1: Laad bestandsnamen uit map (alleen de namen, geen paden)
-dir = 'esc50_extra/train_val_audio'
+dir = 'esc50_extra/bahmei_sounds'
 filenames = set(os.listdir(dir))  # Zorgt voor snelle lookup
 
 # Stap 2: Lees originele CSV in
@@ -13,4 +13,4 @@ df = pd.read_csv(csv_path)
 df = df[df['filename'].isin(filenames)]
 
 # Stap 4: Opslaan naar nieuwe CSV
-df.to_csv('esc50_extra/meta/esc59_train.csv', index=False)
+df.to_csv('esc50_extra/meta/bahmei_sounds.csv', index=False)
